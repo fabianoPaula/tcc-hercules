76b2e2d146a7db484a777ca8f3b759b95ca0da26
Fabiano Martins <fabiano.paula.martins@gmail.com>
2019-07-16 02:05:50 +0200

    Organizing scripts

--- buildCollectors.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python makeCollectors.py 50 4 200 > collect_OLPM.c
-python makeCollectors.py 92 18 200 > collect_battery.c
--- pythonScripts/sumMemoryUsage.py
+++ pythonScripts/sumMemoryUsage.py
@@ -1,12 +1,17 @@
-f = open("./ramprof.txt","r")
+#!/usr/bin/env python
+import sys
 
+if( len(sys.argv) < 3): 
+	print("Should have two arguments")
+	exit(0)
+
+f = open(sys.argv[1],"r")
 sumRamBytes = 0
 for line in f.readlines():
 	aux = int(line.split(" ")[0])
 	sumRamBytes += aux
 
-f = open("./flashprof.txt","r")
-
+f = open(sys.argv[2],"r")
 sumFlashBytes = 0
 for line in f.readlines():
 	aux = int(line.split(" ")[0])
