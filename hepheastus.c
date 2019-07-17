df0a0f6218c3e0a2b1d89d8d8c7ba035e3c2304f
Fabiano Martins <fabiano.paula.martins@gmail.com>
2019-07-17 04:37:19 +0200

    Rewriting a function to use less space

--- hepheastus.c
+++ hepheastus.c
@@ -13,7 +13,6 @@
 #include "net/rpl/rpl.h"
 
 #include <stdio.h>
-#include <string.h>
 
 #define UDP_PORT 1234
 #define SERVICE_ID 190
@@ -22,19 +21,24 @@
 #define POWERTRACE_INTERVAL (2 * CLOCK_SECOND)
 
 #define NUMBER_OF_SAMPLES 200
+#define NUMBER_OF_SPLIT_POINTS 10
 
-// static struct simple_udp_connection hepheastus_connection;
+static struct simple_udp_connection hepheastus_connection;
 
 uint16_t locked = 0;
 
 // Definições dos pontos de cisão
-float split_points[NUMBER_OF_SAMPLES];
+float split_points[NUMBER_OF_SPLIT_POINTS];
 uint16_t split_counter = 0;
 
 float samples[NUMBER_OF_SAMPLES];
 uint16_t samples_counter = 0;
 
 char message_buffer[100];
+
+// void printf_float(float number){
+// 	printf("%ld.%02u", (long) number, (unsigned)((number-myfloor(number))*100));
+// }
 
 float myfloor(float x) {
   if(x >= 0.0f) {
@@ -44,10 +48,6 @@ float myfloor(float x) {
   }
 }
 
-void printf_float(float number){
-	printf("%ld.%02u", (long) number, (unsigned)((number-myfloor(number))*100));
-}
-
 float mysqrt(float number) {
 	float sqrt = 0, i, upper, lower;
 
@@ -55,8 +55,6 @@ float mysqrt(float number) {
 		for( i = 0; i < 255; i++){
 			lower = i*i;
 			upper = (i+1)*(i+1);
-			// printf("%ld.%02u - ", (long) lower, (unsigned)((lower-myfloor(lower))*1000));
-			// printf("%ld.%02u\n", (long) upper, (unsigned)((upper-myfloor(upper))*1000));
 			if( lower <= number && number < upper){
 				sqrt = i;
 				break;
@@ -66,57 +64,10 @@ float mysqrt(float number) {
 		sqrt = 255;
 	}
 	
-	
-	while( sqrt*sqrt < number ){
-		// printf("%ld.%02u - ", (long) number, (unsigned)((number-myfloor(number))*100));
-		// printf("%ld.%02u\n", (long) sqrt, (unsigned)((sqrt-myfloor(sqrt))*100));
+	while( sqrt*sqrt < number )
 		sqrt = sqrt + 0.001;
-	}
 	
 	return sqrt;
-}
-
-uint16_t select_maximun(float *data, uint16_t begin, uint16_t end, float value){
-	uint16_t upinterval = begin;
-	
-	while( data[upinterval] < value && upinterval < (end - 1)){
-		upinterval = upinterval + 1;
-	}
-	
-	return upinterval;
-}
-
-
-float mean(float *y,uint16_t begin,uint16_t end){
-	uint16_t number_of_elements = end - begin, i;
-	float aux = 0;
-	
-	if( end <= begin ){
-		return 0;
-	}
-
-	for(i = begin; i < end; i++){
-		aux = aux + y[i]/number_of_elements;
-	}
-
-	return aux;
-}
-
-float standard_desviation(float *x, uint16_t begin, uint16_t end){
-	float sum_mean = 0, sum_square = 0;
-	uint16_t i;
-
-	if( end <= begin ){
-		return 0;
-	}
-
-	sum_mean = mean(x, begin, end);
-
-	for(i = begin; i < end; i++){
-		sum_square = sum_square + (x[i] - sum_mean)*(x[i] - sum_mean);
-	}
-
-	return mysqrt(sum_square/(end - begin));
 }
 
 float kurtosis(float *x, uint16_t begin, uint16_t end){
@@ -136,30 +87,41 @@ float kurtosis(float *x, uint16_t begin, uint16_t end){
 	return (p75 - p25)/2*(p90 - p10);
 }
 
-float skew(float *x, uint16_t begin, uint16_t end){
-	float mean_value, p50, sd, aux;
 
-	mean_value = mean(x, begin, end);
+void skew_mean(float *x, uint16_t begin, uint16_t end, float *mean, float *skew){
+	float mean_value = 0, p50, sd;
+	uint16_t number_of_elements = end - begin, i;
 
-	if ( (end - begin) % 1 == 1){
+	if( number_of_elements == 0 ) return;
+
+	mean_value = 0;
+
+	for(i = begin; i < end; i++)
+		mean_value = mean_value + x[i]/number_of_elements;
+
+	sd = 0;
+	for(i = begin; i < end; i++)
+		sd = sd + (x[i] - mean_value)*(x[i] - mean_value);
+
+	sd = mysqrt(sd/number_of_elements);
+
+	if ( number_of_elements % 1 == 1){
 		p50 = x[(begin+end)/2];
 	}else{
 		p50 = (x[(begin + end)/2] + x[(begin + end)/2 - 1])/2;
 	}
 	
-	sd = standard_desviation(x, begin, end);
-
-	return 3*(mean_value - p50)/sd;
+	*mean = mean_value;
+	*skew = 3*(mean_value - p50)/sd;
 }
 
 void hepheastus(uint16_t begin, uint16_t end, uint16_t level){
 	uint16_t upper;
 	float skew_value, kurtosis_value, mean_value;
 
-	mean_value = mean(samples, begin, end);
+	skew_mean(samples, begin, end, &mean_value, &skew_value);
 	kurtosis_value = kurtosis(samples, begin, end);
-	skew_value = skew(samples, begin, end);
-
+	
 	if( skew_value < 1){
 		if( kurtosis_value < 0.26 ){
 			printf("Leptokurtic Set! : ");
@@ -167,72 +129,16 @@ void hepheastus(uint16_t begin, uint16_t end, uint16_t level){
 			printf("Platykurtic Set! : ");
 		}
 
-		// printf("(%u.%u,%u.%u,%u.%u)\n",
-		// 	mean_value/100, mean_value % 100,
-		// 	kurtosis_value/100, kurtosis_value % 100,
-		// 	skew_value/100, skew_value % 100
-		// );
-
 	}else{
 		split_points[split_counter++] = mean_value;
 
-		upper = select_maximun(samples, begin, end, mean_value);
+		upper = begin;
+
+		for(; samples[upper] < mean_value && upper < (end - 1); upper++);
 
 		hepheastus(begin, upper, level + 1);
 		hepheastus(upper,   end, level + 1);
 	}
-}
-
-
-
-// Função processo principal
-void hepheastus_main(){
-	uint begin, end, i, j, value;
-
-	// Ordenando vertor de leitura 
-	for(i = 0; i < samples_counter - 1; i++){
-		for(j = i; j < samples_counter; j++){
-			if( samples[i] > samples[j]){
-				value = samples[i];
-				samples[i] = samples[j];
-				samples[j] = value;
-			}
-		}
-	}
-	split_counter = 0;
-	
-	hepheastus(0, samples_counter, 0);
-
-	begin = 0;
-	sprintf(message_buffer, "H:(");
-	// Gerar as mensagens que serão enviadas para as aplicações
-	for(i = 0; i < split_counter; i++){
-		end = select_maximun(samples, 0, samples_counter, split_points[i]);
-		value = mean(samples,begin, end);
-
-		printf("Data[%u] = %u\n", i, value);
-		sprintf(
-			message_buffer, 
-			"%s%u, %ld.%02u),(", 
-			message_buffer, 
-			i,
-			(long) value, 
-			(unsigned)((value-myfloor(value))*100)
-		);
-		begin = end + 1;
-	}
-
-	value = mean(samples,begin, samples_counter);
-	sprintf(
-		message_buffer,
-		"%s%u, %ld.%02u)",
-		message_buffer,
-		i, 
-		(long) value, 
-		(unsigned)((value-myfloor(value))*100)
-	);
-
-	printf("%s\n", message_buffer);	
 }
 
 /*---------------------------------------------------------------------------*/
@@ -267,11 +173,9 @@ receiver(struct simple_udp_connection *c,
 			samples[samples_counter] = 0;
 			pointer = ++i;
 			for(; data[i] != ',' && i < datalen; i++);
-			printf("%d - %d - %d \n", pointer, i, datalen);
 			for(j = pointer; j < i; j++){
 				aux = 1;
 				for(k = j; k < (i - 1); k++) aux *= 10.0;
-				printf("%d - %c\n", aux, data[j]);
 
 				samples[samples_counter] += (data[j] - '0')*aux;
 			}
@@ -331,13 +235,14 @@ create_rpl_dag(uip_ipaddr_t *ipaddr)
 PROCESS_THREAD(unicast_receiver_process, ev, data)
 {
   static struct etimer periodic_timer;
-  static struct etimer send_timer;
   uip_ipaddr_t *ipaddr;
 
   PROCESS_BEGIN();
 
+  uint begin, end, i, j, value, number_of_elements;
+
   // Iniciando o powertrace
-  powertrace_start(POWERTRACE_INTERVAL);
+  // powertrace_start(POWERTRACE_INTERVAL);
 
   // Iniciando o serviço de rede
   servreg_hack_init();
@@ -353,7 +258,7 @@ PROCESS_THREAD(unicast_receiver_process, ev, data)
   // Iniciando o temporizador
   etimer_set(&periodic_timer, SEND_INTERVAL);
 
-  printf("RTIMER_SECOND %u\n", RTIMER_SECOND);
+  // printf("RTIMER_SECOND %u\n", RTIMER_SECOND);
 
   printf("Open collector window\n");
   while(1) {
@@ -364,7 +269,60 @@ PROCESS_THREAD(unicast_receiver_process, ev, data)
 	printf("Open fusion window\n");
 	locked = 1;
 
-	hepheastus_main();
+	// Ordenando vertor de leitura 
+	for(i = 0; i < samples_counter - 1; i++){
+		for(j = i; j < samples_counter; j++){
+			if( samples[i] > samples[j]){
+				value = samples[i];
+				samples[i] = samples[j];
+				samples[j] = value;
+			}
+		}
+	}
+	split_counter = 0;
+	
+	hepheastus(0, samples_counter, 0);
+
+	begin = 0;
+	sprintf(message_buffer, "H:(");
+	// Gerar as mensagens que serão enviadas para as aplicações
+	for(i = 0; i < split_counter; i++){
+		end = begin;
+		for(; samples[end] < split_points[i] && end < (samples_counter - 1); end++);
+
+		value = 0;
+		number_of_elements = end - begin;
+
+		for(i = begin; i < end; i++)
+			value = value + samples[i]/number_of_elements;
+
+		printf("Data[%u] = %u\n", i, value);
+		sprintf(
+			message_buffer, 
+			"%s%u, %ld.%02u),(", 
+			message_buffer, 
+			i,
+			(long) value, 
+			(unsigned)((value-myfloor(value))*100)
+		);
+		begin = end + 1;
+	}
+
+	value = 0;
+	number_of_elements = samples_counter - begin;
+
+	for(i = begin; i < samples_counter; i++)
+		value = value + samples[i]/number_of_elements;
+	
+	sprintf(
+		message_buffer,
+		"%s%u, %ld.%02u)",
+		message_buffer,
+		i, 
+		(long) value, 
+		(unsigned)((value-myfloor(value))*100)
+	);
+	printf("%s\n", message_buffer);	
 
 	printf("Close fusion window\n");
 	locked = 0;
